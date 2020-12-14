075d7b945ee47c240ac05c82fe35d1fd6e9cf73b
yurii <papka.yurii@gmail.com>
2020-12-15 01:32:16 +0200

    add product card page to router

---
+++ src/pages/productCard/index.tsx
@@ -0,0 +1,9 @@
+import React, {FC} from 'react';
+import ImplementMe from "../../inst/pages/ImplementMe";
+
+const ProductCard: FC = () => {
+
+  return <ImplementMe />
+};
+
+export default ProductCard;
