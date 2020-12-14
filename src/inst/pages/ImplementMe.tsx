482a64863ccce7bd0098fcff62dc0e43f282cac5
yurii <papka.yurii@gmail.com>
2020-12-15 00:08:14 +0200

    add products page to next router

---
+++ src/inst/pages/ImplementMe.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from 'react';
+import {Typography} from '@material-ui/core';
+
+const ImplementMe: FC = () => {
+
+  return (
+    <Typography variant={'h3'}>
+      Implement me please!
+    </Typography>
+  );
+};
+
+export default ImplementMe;
---
+++ src/pages/products/index.tsx
@@ -0,0 +1,10 @@
+import React, {FC} from 'react';
+import ImplementMe from "../../inst/pages/ImplementMe";
+
+const ProductList: FC = () => {
+  return (
+    <ImplementMe />
+  );
+};
+
+export default ProductList;
